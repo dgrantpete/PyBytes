cc463ac7fe61be249a4509617cb30fbe5776503d
Grant Peterson <dgrantpete@gmail.com>
2021-08-30 23:45:59 -0600

    Configured "setup.py" to display description on PyPi

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name = "PyBytes",
     version = "1.0.0",
@@ -10,6 +13,8 @@ setup(
     author = 'Grant Peterson',
     author_email = "dgrantpete@gmail.com",
     description = "A library to assist in the creation and manipulation of bytes.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license = "MIT",
     keywords = "byte bytes bit bits binary bin",
     url = "https://github.com/dgrantpete/PyBytes",
